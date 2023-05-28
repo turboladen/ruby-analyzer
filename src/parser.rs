aa5f3ec76ba876d3a5d3a5907d96ca9301f21a8c
Steve Loveless <steve.loveless@gmail.com>
2023-05-27 19:37:51 -0700

    Move tree-sitter to separate module

--- src/parser.rs
+++ src/parser.rs
@@ -49,20 +49,6 @@ pub fn parse(db: &dyn crate::db::Db, file_source: FileSource) -> Arc<Vec<Node>> 
     }
 }
 
-pub fn parse_ts(
-    db: &dyn crate::db::Db,
-    file_source: FileSource,
-) -> std::option::Option<tree_sitter::Tree> {
-    let mut parser = tree_sitter::Parser::new();
-    let code = file_source.code(db);
-
-    parser
-        .set_language(tree_sitter_ruby::language())
-        .expect("Error loading Ruby grammar");
-
-    parser.parse(code.bytes().collect::<Vec<u8>>(), None)
-}
-
 /// Just a wrapper for calling `lib_ruby_parser`'s parse function.
 ///
 pub(crate) fn lrp_parse(buffer_name: &Path, code: &Rope) -> lib_ruby_parser::ParserResult {
@@ -130,96 +116,4 @@ mod tests {
         let diags = parse::accumulated::<Diagnostics>(&db, file_source);
         assert_eq!(diags.len(), 1);
     }
-
-    // Bummer that we don't get any errors/diagnostics from TS.
-    //
-    #[test]
-    fn ts_parse_invalid_code() {
-        let db = crate::db::Database::default();
-        let file_uri = PathBuf::from("/tmp/test.rb");
-        let code = Rope::from_str("class Foo; ");
-
-        let file_source = FileSource::new(&db, file_uri, code.clone());
-        let tree = parse_ts(&db, file_source).unwrap();
-        dbg!(&tree);
-
-        let mut program = tree.walk();
-        assert_eq!(program.node().child_count(), 1);
-        assert_eq!(program.node().named_child_count(), 1);
-
-        assert!(program.goto_first_child());
-        dbg!(program.node());
-        assert_eq!(program.node().kind(), "class");
-        assert!(program.node().has_error());
-        assert_eq!(program.node().child_count(), 4);
-        assert_eq!(program.node().named_child_count(), 1);
-
-        assert!(program.goto_first_child());
-        dbg!(program.node());
-        assert!(!program.node().has_error());
-
-        assert!(program.goto_next_sibling());
-        dbg!(program.node());
-        assert!(!program.node().has_error());
-
-        assert!(program.goto_next_sibling());
-        dbg!(program.node());
-        assert!(!program.node().has_error());
-
-        assert!(program.goto_next_sibling());
-        dbg!(program.node());
-        assert!(program.node().has_error());
-
-        //  (#eq? @name "Foo")
-        let query =
-            // tree_sitter::Query::new(tree_sitter_ruby::language(), "(program (class))").unwrap();
-            tree_sitter::Query::new(
-                tree_sitter_ruby::language(),
-                r#"[
-  (class
-    name: [
-      (constant) @name
-      (scope_resolution
-        name: (#eq? @name "Foo"))
-    ]) @definition.class
-  (singleton_class
-    value: [
-      (constant) @name
-      (scope_resolution
-        name: (#eq? @name "Foo"))
-    ]) @definition.class
-]"#
-            ).unwrap();
-
-        let mut query_cursor = tree_sitter::QueryCursor::new();
-        let c = code.to_string();
-        let matches = query_cursor.matches(&query, tree.root_node(), c.as_bytes());
-
-        for m in matches {
-            if m.captures.is_empty() {
-                continue;
-            }
-            dbg!(&m);
-            for capture in m.captures {
-                dbg!(capture.node);
-            }
-        }
-
-        panic!("hinm");
-    }
-
-    // #[test]
-    // fn ts_parse_valid_ruby_test() {
-    //     let db = crate::db::Database::default();
-    //     let file_uri = PathBuf::from("/tmp/test.rb");
-    //     let code = Rope::from_str("class Foo; end");
-
-    //     let file_source = FileSource::new(&db, file_uri, code);
-
-    //     let nodes = parse(&db, file_source);
-    //     assert_eq!(2, nodes.len());
-
-    //     let nodes = parse(&db, file_source);
-    //     assert_eq!(2, nodes.len());
-    // }
 }
